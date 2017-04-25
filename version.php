3ddd019781d482e6a145a78e0cd966ff7bc4f871
Arnaud Trouve <ak4t0sh@free.fr>
2017-04-25 18:57:12 +0200

    update version number

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_codepen'; // Full name of the plugin (used for diagnostics)
-$plugin->version   = 2017042400;       // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2017042500;       // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016052300;       // Requires this Moodle version
 $plugin->release   = 'v3.1-r1';       // Human-friendly version name http://docs.moodle.org/dev/Releases
 $plugin->maturity  = MATURITY_STABLE;  // This version's maturity level.
